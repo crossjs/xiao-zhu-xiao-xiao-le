d3669cb31be6dfea8f0d3e9693dc7d41a3682610
crossjs <liwenfu@crossjs.com>
2019-03-13 21:03:16 +0800

    update

--- src/game/components/Model.ts
+++ src/game/components/Model.ts
@@ -76,63 +76,8 @@ namespace game {
       return num;
     }
 
-    /**
-     * 寻找可合并的数字链
-     * @param firstNumber 优先合并的数字
-     */
-    public getChain(firstNumber: number): [number, Point[]] {
-      const numMap: { [num: string]: Point[] } = {};
-      yyw.traverseMatrix(this.matrix, (num: number, point) => {
-        if (num !== MAGIC_NUMBER) {
-          if (!numMap[`${num}`]) {
-            numMap[`${num}`] = [];
-          }
-          numMap[`${num}`].push(point);
-        }
-      });
-      const entries = Object.entries(numMap);
-      if (firstNumber) {
-        // 指定的数字先检查
-        entries.sort(([num]) => +num === firstNumber ? -1 : 1);
-      }
-      for (const [num, points] of entries) {
-        if (points.length < 3) {
-          continue;
-        }
-        // 剔除不存在邻居的点
-        const filteredPoints = points.filter((p) => !!getNeighborOf(p, points));
-        for (let i = 0; i < filteredPoints.length; i++) {
-          const neighbors = [filteredPoints[i]];
-          // 走 N-1 遍，避免漏网，比如 U 型结构
-          for (let k = 0; k < filteredPoints.length - 1; k++) {
-            for (let j = 0; j < filteredPoints.length; j++) {
-              if (i !== j && neighbors.indexOf(filteredPoints[j]) === -1) {
-                if (neighbors.some((n) => isNeighborPoints(filteredPoints[j], n))) {
-                  neighbors.push(filteredPoints[j]);
-                }
-              }
-            }
-          }
-          if (neighbors.length >= 3) {
-            return [+num, neighbors];
-          }
-        }
-      }
-      return [0, []];
-    }
-
     private createMatrix() {
       this.matrix = [];
-      // if (DEBUG) {
-      //   this.matrix = [
-      //     [4, 3, 3, 4, 3],
-      //     [3, 19, 19, 7, 19],
-      //     [3, 3, 99, 3, 3],
-      //     [4, 4, 3, 4, 4],
-      //     [3, 2, 4, 2, 3],
-      //   ];
-      //   return;
-      // }
       for (let row = 0; row < ROWS; row++) {
         this.matrix[row] = [];
         let num: number;
@@ -204,13 +149,5 @@ namespace game {
 
   export function isNeighborPoints(point1: Point, point2: Point): boolean {
     return Math.abs(point1[0] - point2[0]) + Math.abs(point1[1] - point2[1]) === 1;
-  }
-
-  function getNeighborOf(point: Point, points: Point[]): Point {
-    for (const p of points) {
-      if (isNeighborPoints(p, point)) {
-        return p;
-      }
-    }
   }
 }
